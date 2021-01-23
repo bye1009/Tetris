5f2a9029c4aa882a198f0f168a14d289bdfa8c6b
yin <1275817506@qq.com>
2021-01-23 20:07:40 +0800

    first commit

---
+++ src/tetris/Main.java
@@ -0,0 +1,13 @@
+package tetris;
+
+/**
+ * 程序入口函数
+ *
+ * @param args String[],附带的命令行参数
+ */
+
+public class Main {
+	public static void main(String[] args) {
+        new Tetris("俄罗斯方块");
+    }
+}
